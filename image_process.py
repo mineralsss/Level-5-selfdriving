353c3633f68636e0b26ff9502754d3498491ca67
minerals <42133320+mineralsss@users.noreply.github.com>
2024-03-06 20:27:33 +0700

    Delete image_process.py

--- image_process.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-import cv2
-def tracking():
-    model = YOLO(r"c:\Users\tuana\Downloads\yolov8m.pt")
-    result = model.track(source = "Euro Truck Simulator 2 1_19_2024 10_12_02 AM.png", show = True, tracker = "bytetrack.yaml")
-    cv2.waitKey(0)
-tracking()
